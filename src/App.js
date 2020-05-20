f6ce20108fab13290a70426a875b43fbf5705c71
Alun Turner <alun.turner@googlemail.com>
2020-05-20 18:32:59 +0100

    starting on material ui work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Title from "./components/Title";
 import GoogleMap from "./components/GoogleMap";
-import database from "./firebaseInitialise";
+// import database from "./firebaseInitialise";
 
 function App() {
   return (
