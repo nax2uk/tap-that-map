250b63eda6b3a7957ffe72ff58d7e46d5ae94853
Alun Turner <alun.turner@googlemail.com>
2020-05-21 18:40:57 +0100

    restyle map

--- src/Data/mapStyling.js
+++ src/Data/mapStyling.js
@@ -1,43 +1,30 @@
 const mapStyle = [
   {
-    featureType: "all",
-    stylers: [{ color: "#277321" }],
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
   },
-  { featureType: "water", stylers: [{ color: "#58dbd3" }] },
+  {
+    featureType: "water",
+    stylers: [
+      {
+        color: "#081f8c",
+      },
+      {
+        visibility: "on",
+      },
+    ],
+  },
   {
     featureType: "water",
     elementType: "geometry.stroke",
-    stylers: [{ color: "#58dbd3" }],
-  },
-  {
-    featureType: "all",
-    elementType: "labels",
-    stylers: [{ visibility: "off" }],
-  },
-  { featureType: "road", stylers: [{ visibility: "off" }] },
-  {
-    featureType: "water",
-    elementType: "labels",
-    stylers: [{ visibility: "off" }],
-  },
-  {
-    featureType: "administrative.country",
-    elementType: "geometry.stroke",
-    stylers: [{ visibility: "off" }],
-  },
-  {
-    featureType: "administrative.province",
-    elementType: "geometry.stroke",
-    stylers: [{ visibility: "off" }],
-  },
-  {
-    featureType: "administrative.land_parcel",
-    elementType: "geometry.stroke",
-    stylers: [{ visibility: "off" }],
-  },
-  {
-    featureType: "transit",
-    stylers: [{ visibility: "off" }],
+    stylers: [
+      {
+        color: "#000000",
+      },
+    ],
   },
 ];
 
