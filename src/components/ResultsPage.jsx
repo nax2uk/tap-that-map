49ad48d03c3adbd3ea17b8e84df9ec90716fa7a8
Alun Turner <alun.turner@googlemail.com>
2020-05-25 21:06:24 +0100

    added link to homepage from results page

--- src/components/ResultsPage.jsx
+++ src/components/ResultsPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "@reach/router";
 
 const ResultsPage = ({ scoreArr, totalScore }) => {
   return (
@@ -7,37 +8,20 @@ const ResultsPage = ({ scoreArr, totalScore }) => {
       <h2>Here are your results:</h2>
       <ul>
         <li>Round 1:{scoreArr[0]}</li>
-      </ul>
-      <ul>
         <li>Round 2:{scoreArr[1]}</li>
-      </ul>
-      <ul>
         <li>Round 3:{scoreArr[2]}</li>
-      </ul>
-      <ul>
         <li>Round 4:{scoreArr[3]}</li>
-      </ul>
-      <ul>
         <li>Round 5:{scoreArr[4]}</li>
-      </ul>
-      <ul>
         <li>Round 6:{scoreArr[5]}</li>
-      </ul>
-      <ul>
         <li>Round 7:{scoreArr[6]}</li>
-      </ul>
-      <ul>
         <li>Round 8:{scoreArr[7]}</li>
-      </ul>
-      <ul>
         <li>Round 9:{scoreArr[8]}</li>
-      </ul>
-      <ul>
         <li>Round 10:{scoreArr[9]}</li>
-      </ul>
-      <ul>
         <li>Total: {totalScore}</li>
       </ul>
+      <button>
+        <Link to="/">BACK HOME</Link>
+      </button>
     </div>
   );
 };
