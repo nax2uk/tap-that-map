09a73148b82599e1e31008d73b00516d6f3a7150
Azlina Yeo <azlinayeo@Azlinas-MBP.lan>
2020-05-26 16:54:33 +0100

    added ErrorMessage component

---
+++ src/components/ErrorMessage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorMessage = (props) => {
+    const { message } = props;
+    return (
+        <div>
+            {message}
+        </div>
+    );
+};
+
+export default ErrorMessage;
