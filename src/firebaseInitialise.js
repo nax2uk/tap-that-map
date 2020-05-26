afb563f2354b2ade85f358e68e6bc26784e1f3e6
Azlina Yeo <azlinayeo@Azlinas-MBP.lan>
2020-05-26 20:08:46 +0100

    deleted commented out code

--- src/firebaseInitialise.js
+++ src/firebaseInitialise.js
@@ -1,15 +1,4 @@
-/*import firebaseConfig from "./firebaseConfig";
-import * as firebase from "firebase";
-
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
-export const auth = firebase.auth();
-
-export default database;
-*/
-
 import firebase from 'firebase/app'; // firebase utility library
-//import 'firebase/firestore';
 import 'firebase/firebase-auth';
 import 'firebase/database';
 import firebaseConfig from './firebaseConfig';
@@ -18,10 +7,6 @@ const config = firebaseConfig;
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
-//export const firestore = firebase.firestore();
 export const database = firebase.database();
-//const provider = new firebase.auth.GoogleAuthProvider();
-//provider.setCustomParameters({ prompt: 'select_account' });
-//export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
