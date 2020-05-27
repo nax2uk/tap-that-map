8e48c0919963ef8e4f9bbe2c6fc85cae96b0fafb
Alun Turner <alun.turner@googlemail.com>
2020-05-27 21:10:59 +0100

    tinkered with transition times on nextbutton.jsx to avoid multiple clicks

--- src/components/NextButton.jsx
+++ src/components/NextButton.jsx
@@ -6,7 +6,11 @@ import theme from "../resources/theme.jsx";
 const NextButton = ({ updateRound, round, gameIsRunning, roundIsRunning }) => {
   return (
     <ThemeProvider theme={theme}>
-      <Slide direction="left" in={gameIsRunning && !roundIsRunning}>
+      <Slide
+        direction="left"
+        in={gameIsRunning && !roundIsRunning}
+        timeout={{ enter: 100, exit: 50 }}
+      >
         <Paper elevation={3} id="next-wrapper">
           <Button variant="contained" color="primary" onClick={updateRound}>
             <Typography variant="h4">
