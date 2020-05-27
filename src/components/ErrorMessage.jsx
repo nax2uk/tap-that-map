d8855570c0bc5be5f94766a67002baa68d2e5529
Alun Turner <alun.turner@googlemail.com>
2020-05-27 19:19:00 +0100

    applied material-ui to the error messages

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Box, Typography } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "../resources/theme.jsx";
 
-const ErrorMessage = (props) => {
-    const { message } = props;
-    return (
-        <div>
-            {message}
-        </div>
-    );
+const ErrorMessage = ({ message }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Box>
+        <Typography variant="body1">{message}</Typography>
+      </Box>
+    </ThemeProvider>
+  );
 };
 
 export default ErrorMessage;
